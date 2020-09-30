820745a01ac9d399c836903a73719a8becc70b45
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-09-30 23:28:28 +0300

    let,var keyword

--- scope.js
+++ scope.js
@@ -3,20 +3,20 @@ var sehir="Ankara";
 sayi=11;
 sehir="Adana";
 
- var sehir="İzmir";
+ var sehir="İzmir"; //Aynı sehir i birdaha tanımladı ve uyarı vermedi
 
 
- let il="Elazığ";
+ let il="Elazığ";  //let keyword ile tanımlamak daha yaygınlaşmış bir durum.
  il="Hatay";
 
-//  let il="Kocaeli";
+//  let il="Kocaeli"; Hata verecek il daha önemli tanımlı diyecek
 
  function mesajVer(){
      var isim="Neslihan";
      console.log(sehir);
      console.log(isim);
      
- }
+ } 
     
 // console.log(sayi);
 // console.log(sehir);
